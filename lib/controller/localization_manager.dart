7580e9fed9d2b3bd0b8d95dd3e846b69db64470f
Ricarth Lima <ricarth.lima@gmail.com>
2024-11-12 14:46:29 -0300

    v3.1 - gerenciador de idiomas

---
+++ lib/controller/localization_manager.dart
@@ -0,0 +1,29 @@
+class LocalizationManager {
+  String languageCode;
+  LocalizationManager(this.languageCode);
+
+  static const String defaultLanguageCode = "en";
+
+  // No futuro esse map tem que se preencher automagicamente
+  final Map<String, Map<String, String>> _mapLanguages = {
+    "pt": {
+      "clearBooks": "Limpar todos os livros",
+    },
+    "es": {
+      "clearBooks": "Eliminar todos los libros",
+    },
+    "en": {
+      "clearBooks": "Clear all books",
+    },
+  };
+
+  String _getSentence(String keySentence) {
+    String? sentence = _mapLanguages[languageCode]![keySentence];
+
+    sentence ??= _mapLanguages[defaultLanguageCode]![keySentence]!;
+
+    return sentence;
+  }
+
+  String get clearBooks => _getSentence("clearBooks");
+}
